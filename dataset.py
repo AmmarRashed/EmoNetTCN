61c37bfa49b883a0ca9d3e0e8a07794dbbf42c50
ammarrashed <ammarrashed54@gmail.com>
2023-05-12 12:54:12 -0400

    face detection with fast mtcnn

--- dataset.py
+++ dataset.py
@@ -5,6 +5,8 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_video
 
+from face_detection import detect_faces, FastMTCNN
+
 
 def get_path(clip_id, root, ttv):
     participant_id = clip_id[:6]
@@ -17,7 +19,7 @@ def get_path(clip_id, root, ttv):
 
 
 class VideoDataset(Dataset):
-    def __init__(self, root, csv, ttv, transform=None):
+    def __init__(self, root, csv, ttv, face_detector=FastMTCNN()):
         self.root = root
         self.csv = csv
         self.ttv = ttv  # train test or validation
@@ -26,7 +28,7 @@ class VideoDataset(Dataset):
             path=self.paths_df.ClipID.apply(get_path, root=root, ttv=ttv)
         ).dropna()
         self.paths_df.columns = [c.strip() for c in self.paths_df.columns]  # some columns have extra spaces
-        self.transform = transform
+        self.face_detector = face_detector
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     def __len__(self):
@@ -34,12 +36,11 @@ class VideoDataset(Dataset):
 
     def __getitem__(self, idx):
         clip = self.paths_df.iloc[idx]
-        video, audio, info = read_video(clip.path)
-        if self.transform:
-            video = self.transform(video)
-        video = video.to(self.device)
-        return dict(video=video,
-                    boredom=clip.Boredom,
-                    engagement=clip.Engagement,
-                    confusion=clip.Confusion,
-                    frustration=clip.Frustration)
+        faces = detect_faces(self.face_detector, clip.path, max_frames=300)
+        return dict(
+            clip_id=clip.ClipID,
+            faces=faces,
+            boredom=clip.Boredom,
+            engagement=clip.Engagement,
+            confusion=clip.Confusion,
+            frustration=clip.Frustration)
---
+++ face_detection.py
@@ -0,0 +1,63 @@
+import cv2
+import torch
+from facenet_pytorch import MTCNN
+from imutils.video import FileVideoStream
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class FastMTCNN(object):
+    """Fast MTCNN implementation."""
+
+    def __init__(self, stride=1, batch_size=60, image_size=256, resize=1, *args, **kwargs):
+        """Constructor for FastMTCNN class.
+
+        Arguments:
+            stride (int): The detection stride. Faces will be detected every `stride` frames
+                and remembered for `stride-1` frames.
+
+        Keyword arguments:
+            resize (float): Fractional frame scaling. [default: {1}]
+            these parameters are already set for MTCNN:
+            select_largest=True and post_process=False
+            *args: Arguments to pass to the MTCNN constructor. See help(MTCNN).
+            **kwargs: Keyword arguments to pass to the MTCNN constructor. See help(MTCNN).
+        """
+        self.stride = stride
+        self.resize = resize
+        self.batch_size = batch_size
+        self.mtcnn = MTCNN(image_size=image_size, select_largest=True, post_process=False, *args, **kwargs)
+
+    def __call__(self, frames):
+        """Detect faces in frames using strided MTCNN."""
+        if self.resize != 1:
+            frames = [
+                cv2.resize(f, (int(f.shape[1] * self.resize), int(f.shape[0] * self.resize)))
+                for f in frames
+            ]
+
+        faces_subset = self.mtcnn(frames[::self.stride])
+
+        faces = []
+        for i, face in enumerate(faces_subset):
+            faces += [face] * self.stride
+
+        return faces
+
+
+def detect_faces(fast_mtcnn, filename, max_frames=300):
+    frames = []
+    faces = []
+    batch_size = 60
+    v_cap = FileVideoStream(filename).start()
+    v_len = min(max_frames, int(v_cap.stream.get(cv2.CAP_PROP_FRAME_COUNT)))
+    # print(f"Total of {v_len} frames.")
+    for j in range(v_len):
+        frame = v_cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frames.append(frame)
+        if len(frames) >= batch_size or j == v_len - 1:
+            faces += fast_mtcnn(frames)
+            frames = []
+    v_cap.stop()
+    return torch.stack(faces, dim=0)
